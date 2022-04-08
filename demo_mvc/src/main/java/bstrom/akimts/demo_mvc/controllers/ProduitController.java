73e44ce994212e7abac1c62d2ccbca3deffe49b7
Crespin <crespin.christophe@netc.eu>
2022-04-08 14:49:20 +0200

    Commit avant pratique

--- demo_mvc/src/main/java/bstrom/akimts/demo_mvc/controllers/ProduitController.java
+++ demo_mvc/src/main/java/bstrom/akimts/demo_mvc/controllers/ProduitController.java
@@ -1,11 +1,15 @@
 package bstrom.akimts.demo_mvc.controllers;
 
 import bstrom.akimts.demo_mvc.models.Produit;
+import bstrom.akimts.demo_mvc.models.ProduitForm;
 import bstrom.akimts.demo_mvc.service.ProduitService;
+//import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -19,30 +23,55 @@ public class ProduitController {
         this.service = service;
     }
 
+    /*@ModelAttribute("get_all")
+    public List<Produit> getAll(){
+        return service.getAll();
+    }à revoir et th:with qui permet de déclarer une variable dans html*/
+
     @GetMapping("/{id}")
+    //@PreAuthorize("permitAll()")
     public String displayOne(@PathVariable int id, Model model){
         Produit p = service.getOne(id);
         model.addAttribute("produit", p);
-        return "displayOne";
+        return "/pages/displayOne";
     }
 
     @GetMapping
+    //@PreAuthorize("permitAll()")
     public String displayAll(Model model){
         List<Produit> list = service.getAll();
         model.addAttribute("liste_produit", list);
-        return "displayAll";
+        return "/pages/displayAll";
     }
 
     @GetMapping("/marque")
+    //@PreAuthorize("permitAll()")
     public String displayByMarque(@RequestParam String marque, Model model){
-        List<Produit> list = service.getByMarque(marque);
-        model.addAttribute("liste_marque", list);
-        return "displayByMarque";
+        //List<Produit> list = service.getByMarque(marque);
+        model.addAttribute("liste_marque", service.getByMarque(marque));
+        model.addAttribute( "marque", marque);//pas nécessaire fct sans mais permet de fct même si la liste est vide
+        return "/pages/displayByMarque";
+    }
+
+    @GetMapping("/add")
+    //@PreAuthorize("isAuthenticated()")
+    public String displayInsertForm(@ModelAttribute("produit") ProduitForm form){
+        return "forms/produitForm";
+    }
+
+    @PostMapping("/add")
+   // @PreAuthorize("isAuthenticated()")
+    public String processInsert(@Valid @ModelAttribute("produit") ProduitForm form, BindingResult binding){
+        if(binding.hasErrors())
+            return"forms/produitForm";
+
+        Produit rslt = service.insert(form);
+        return "redirect:/produit/" + rslt.getId();
     }
 
     @ExceptionHandler(NoSuchElementException.class)
     public String handle(NoSuchElementException ex){
-        return "404";
+        return "/pages/404";
     }
 
 }
---
+++ demo_mvc/src/main/java/bstrom/akimts/demo_mvc/models/ProduitForm.java
@@ -0,0 +1,24 @@
+package bstrom.akimts.demo_mvc.models;
+
+
+import lombok.Data;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+public class ProduitForm {
+
+    @NotBlank
+    @Size(min = 2, max = 50)
+    private String marque;
+    @NotBlank
+    @Size(min = 2, max = 50)
+    private String modele;
+    @NotNull
+    @Min(0)
+    private Double prix;
+
+}
